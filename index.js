553d33d4ce581cd030ed38345ea05512fd68a2cc
[itsthejdp] <[joegross116@gmail.com]>
2021-09-28 14:17:07 +0800

    first commit PC 9/28 2:15 pm

---
+++ index.js
@@ -0,0 +1,28 @@
+class Post {
+  constructor(title, type) {
+    this.title = title;
+    this.type = type;
+  }
+}
+
+class userAccount {
+  constructor()
+}
+``
+class JavaBlog extends userAccount {
+
+}
+
+let introductory = new Post("Hello everyone!", "general");
+
+
+/*
+
+
+if(title === '' || author === '' || isbn === '') {
+
+  //Show success message
+  UI.showAlert('Book Added', 'success')
+}
+
+*/
