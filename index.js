d373fb4d7e808fab3268509e74faa162b360caf9
[itsthejdp] <[joegross116@gmail.com]>
2021-09-30 13:28:05 +0800

    Changes via PC 1330

--- index.js
+++ index.js
@@ -9,12 +9,37 @@ if(title === '' || author === '' || isbn === '') {
 
 */
 
-class Task {
-  constructor(taskName, day, category) {
-    this.taskName = taskName;
-    this.day = day;
+
+/* Ideal features
+  1. Must have option to modify reps and sets
+  2. Must have option to 
+
+*/
+
+
+// Exercise Class: represents a workout
+class Exercise {
+  constructor(exerciseName, bodyPart, category) {
+    this.exerciseName = exerciseName;
+    this.bodyPart = bodyPart;
     this.category = category;
   }
 }
 
-class ListUI {}
+//UI class: handle UI tasks
+class UI {
+  static displayExercises() {
+    const exercises = 
+  }
+}
+
+//Storage class: handles storage
+class Storage {
+
+}
+
+//Event: Display Exercises
+
+//Event: Add an Exercise
+
+//Event: Remove an Exercise
